e99ada84eb5c81be5bb41c419c92475e37e299dd
NNASTYA2007 <nedyaknast@gmail.com>
2023-11-05 13:25:27 +0100

    HW

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,8 @@
-function makeMessage(name, price) {
-  return `You picked ${name}, price per item is ${price}`;
+function makeTransaction(quantity, pricePerDroid) {
+  return `You ordered ${quantity} droids worth ${
+    quantity * pricePerDroid
+  } credits!`;
 }
-makeMessage("Radar", 6150);
-makeMessage("Scanner", 3500);
-makeMessage("Reactor", 8000);
-makeMessage("Engine", 4070);
+console.log(makeTransaction(5, 3000)); //"You ordered 5 droids worth 1500 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
