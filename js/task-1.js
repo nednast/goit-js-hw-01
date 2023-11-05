d893f086338d0461bf24c99a35e74c22eb04297b
NNASTYA2007 <nedyaknast@gmail.com>
2023-11-05 12:20:03 +0100

    HW

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+function makeMessage(name, price) {
+  return `You picked ${name}, price per item is ${price}`;
+}
+makeMessage("Radar", 6150);
+makeMessage("Scanner", 3500);
+makeMessage("Reactor", 8000);
+makeMessage("Engine", 4070);
