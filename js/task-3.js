9c26943847fcabebc95f935cfe929d96d44b9d47
NNASTYA2007 <nedyaknast@gmail.com>
2023-11-05 14:10:26 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,8 @@
 function getElementWidth(content, padding, border) {
   return (
     Number.parseFloat(content) +
-    Number.parseFloat(padding) +
-    Number.parseFloat(border) * 4
+    Number.parseFloat(padding) * 2 +
+    Number.parseFloat(border) * 2
   );
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
